dc4b2f69576a0b2a2d1ad5b7d3dc94abf35a3742
David Mebo <mebodave@gmail.com>
2025-03-30 08:52:16 +0100

    initial if statement practice

--- chapter2/main.go
+++ chapter2/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -31,14 +32,39 @@ import (
 // }
 
 // else if
-func main() {
-	input := 10
-	// input := -11
-	// input := 15
+// func main() {
+// 	input := 10
+// 	// input := -11
+// 	// input := 15
 
+// 	if input < 0 {
+// 		fmt.Println("input cannot be negative")
+// 	} else if input%2 == 1 {
+// 		fmt.Println(input, "is odd")
+// 	} else {
+// 		fmt.Println(input, "is even")
+// 	}
+// }
+
+// initial if statement
+func validate(input int) error {
 	if input < 0 {
-		fmt.Println("input cannot be negative")
-	} else if input%2 == 1 {
+		return errors.New("input cannot be a negative number")
+	} else if input > 100 {
+		return errors.New("input cannot be over 100")
+	} else if input%7 == 0 {
+		return errors.New("input cannot be divisible by 7")
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	input := 21
+
+	if err := validate(input); err != nil {
+		fmt.Println(err)
+	} else if input%2 == 0 {
 		fmt.Println(input, "is odd")
 	} else {
 		fmt.Println(input, "is even")
