846dd9c62839fa2e1accd6250108bfa17c81cd4f
David Mebo <mebodave@gmail.com>
2025-04-12 22:45:45 +0100

    update on culprit

--- printermodule/main.go
+++ printermodule/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/sicoyle/printer"
 )
 
+// update: reading the docs this time proved to be better this time...
+// not gonna change this, on another project yes, an example? nah
 func main() {
 	msg := printer.PrintNewUID()
 	fmt.Println(msg)
