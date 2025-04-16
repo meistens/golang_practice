0173db055aeaeaae8069e053775f4e40e1cde7eb
David Mebo <mebodave@gmail.com>
2025-04-16 23:46:40 +0100

    forgot main function

--- db/main.go
+++ db/main.go
@@ -6,26 +6,29 @@ package main
 // honestly, anything that makes using em easier, not make me feel
 // like an insufferable tool...
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
-db, err := sql.Open("postgres_go_test", "user=postgres password=password host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
+func main() {
+	db, err := sql.Open("postgres", "user=postgres password=password host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
 
-if err != nil {
-	panic(err)
-} else {
-	fmt.Println("connection successful")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("connection successful")
+	}
+
+	// ping db
+	connectivity := db.Ping()
+	if connectivity != nil {
+		panic(err)
+	} else {
+		fmt.Println("still connected and ready for req...")
+	}
+
+	// close connection
+	db.Close()
 }
-
-// ping db
-connectivity := db.Ping()
-if connectivity != nil {
-	panic(err)
-} else {
-	fmt.Println("still connected and ready for req...")
-}
-
-// close connection
-db.Close()
